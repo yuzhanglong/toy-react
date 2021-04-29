bbd6dd30c63a20e08e4deb311f9cf9a1fcf6b223
yuzhanglong <yuzl1123@163.com>
2021-04-30 00:00:21 +0800

    :sparkles: feat: finish React.render and React.createElement

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,5 +1,90 @@
-describe('test index', () => {
-  test('temp', () => {
-    console.log('hello world');
+import { React } from '../src';
+
+describe('React.createElement()', () => {
+  test('common usage', () => {
+    const data = React.createElement('div', {
+      name: 'yzl',
+      age: 20,
+    });
+
+    expect(data).toStrictEqual({
+      props: {
+        name: 'yzl',
+        age: 20,
+        children: [],
+      },
+      type: 'div',
+    });
+  });
+
+  test('test nested usage', () => {
+    const data = React.createElement('div', null,
+      React.createElement('h1', {
+        children: [],
+      }, 'first'),
+      React.createElement('h1', {
+        children: [],
+      }, 'second'),
+    );
+
+    expect(data).toStrictEqual({
+      'props': {
+        'children': [
+          {
+            'props': {
+              'children': [
+                {
+                  'props': {
+                    'children': [],
+                    'nodeValue': 'first',
+                  },
+                  'type': 'TEXT',
+                },
+              ],
+            },
+            'type': 'h1',
+          },
+          {
+            'props': {
+              'children': [
+                {
+                  'props': {
+                    'children': [],
+                    'nodeValue': 'second',
+                  },
+                  'type': 'TEXT',
+                },
+              ],
+            },
+            'type': 'h1',
+          },
+        ],
+      },
+      'type': 'div',
+    });
+  });
+});
+
+describe('React.render()', () => {
+  test('test text node, we dont create tag but only text', () => {
+    const data = React.createElement('div', null, 'hello world');
+    const base = document.createElement('div');
+    document.body.appendChild(base);
+    React.render(data, base);
+    expect(base.innerHTML).toStrictEqual('<div>hello world</div>');
+  });
+
+  test('common usage', () => {
+    const data = React.createElement('div', null,
+      'hello',
+      React.createElement('h1', null, 'yzl'),
+    );
+
+    const base = document.createElement('div');
+    document.body.appendChild(base);
+
+    React.render(data, base);
+
+    expect(base.innerHTML).toEqual(`<div>hello<h1>yzl</h1></div>`);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,90 @@
-console.log('hello world')
+const TEXT_ELEMENT = 'TEXT';
+
+type ReactProps = {
+  [key: string]: any
+} & {
+  children?: ReactNode[]
+}
+
+interface ReactNode {
+  type: string
+  props: ReactProps
+}
+
+/**
+ * 创建一个 text 节点
+ *
+ * @param text 一个字符串，表示节点内容
+ */
+export function createTextElement(text: string): ReactNode {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      children: [],
+      nodeValue: text,
+    },
+  };
+}
+
+/**
+ * 创建一个 react element
+ *
+ * @param type dom 节点的类型
+ * @param props element props
+ * @param children react nodes
+ * @return {ReactNode} 一个 react 节点对象
+ */
+function createElement(type: string, props: ReactProps, ...children: (ReactNode | string)[]): ReactNode {
+  const getChildren = () => {
+    return children.map(child => {
+      if (typeof child === 'object') {
+        return child;
+      } else {
+        return createTextElement(child);
+      }
+    });
+  };
+
+  return {
+    type: type,
+    props: {
+      ...props,
+      children: getChildren(),
+    },
+  };
+}
+
+/**
+ * 给定一个 react 节点 和容纳的 HTML Element, 渲染相应的 react 元素
+ *
+ * @param element React 节点对象
+ * @param container 容器
+ */
+function render(element: ReactNode, container: HTMLElement | Text) {
+  const isDomProps = (key) => {
+    return key !== 'children';
+  };
+
+  let DOMElement = element.type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(element.type);
+
+  // 挂载必要的属性到 DOM 上
+  const props = element.props;
+  Object
+    .keys(props)
+    .filter(key => isDomProps(key))
+    .forEach(k => {
+      DOMElement[k] = props[k];
+    });
+
+  // 递归子节点
+  element.props.children.map(res => {
+    render(res, DOMElement);
+  });
+
+  container.appendChild(DOMElement);
+}
+
+export const React = {
+  createElement,
+  render,
+};
